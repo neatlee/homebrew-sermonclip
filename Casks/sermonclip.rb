2bda4d83b805f25d2def5b645d888c81cd647a4f
Mike Lee <mikelee@Mikes-MacBook-Pro.local>
2026-09-18 09:31:24 -0400

    Refresh 1.2.2 DMG checksum

--- Casks/sermonclip.rb
+++ Casks/sermonclip.rb
@@ -1,6 +1,6 @@
 cask "sermonclip" do
   version "1.2.2"
-  sha256 "ddba25830296199453cdef06f943a1878912506003f36e99c90a418879a81a08"
+  sha256 "f55d7236a2f62770ad7bbca36d1c9b4fb660b571d5eadc1171e4da32c0bab9ab"
 
   url "https://github.com/neatlee/SermonClip/releases/download/v#{version}/SermonClip-#{version}-arm64.dmg"
   name "SermonClip"
